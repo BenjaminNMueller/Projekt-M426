0f776a559ca7e0bb1994a90af80590a3263c7b8a
BenjaminNMueller <46718105+BenjaminNMueller@users.noreply.github.com>
2020-05-26 14:54:54 +0200

    Add files via upload
    PHP Data file created and output as json defined

---
+++ data.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/styles.css">
+    </head>
+    <body style = "background-color:grey;">
+<?php
+    header('Content-Type: application/json; charset=utf-8');
+	$servername = "localhost";
+	$user = "quotout";
+	$pw = "qu0t_";
+    $db = "citation";
+
+	$con = new mysqli($servername, $user, $pw, $db);
+
+	if($con->connect_error) {
+	   die("-1".$con->connect_error);
+	}
+    $zufallszahl = rand(1,10);
+    $sql = "SELECT * FROM citation WHERE ID = ". $zufallszahl;
+    $res = $con->query($sql);
+        
+    if($res->num_rows > 0) {
+        while($i = $res->fetch_assoc()){
+            print json_encode('<h1>'.$i ["quote"].'</h1>');
+            print json_encode ('<h3>'.$i ["author"].'</h3>');
+        }
+    }
+    else{
+        print json_encode("-1");
+    }
+    
+    $con->close();
+?>
+    </body>
+</html>
