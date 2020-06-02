570d2c729e0cff2ea043ba2fc5b1fb14cc16a426
DMG03 <64084493+DMG03@users.noreply.github.com>
2020-06-02 14:07:52 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+    $.getJSON("http://localhost/Modul426/data.php", (zitat) => {
+            $(".quote").text(zitat.quote);
+            $(".author").text(zitat.author);
+    });
     setInterval(function(){
         $.getJSON("http://localhost/Modul426/data.php", (zitat) => {
             $(".quote").text(zitat.quote);
